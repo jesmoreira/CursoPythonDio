113210b1a21f657c245b03bdc0e0624978a5d914
jesmoreira <jessicamakani@gmail.com>
2022-01-02 20:02:13 -0300

    Primeiro Commit

---
+++ aula2.py
@@ -0,0 +1,12 @@
+a = int(input('Entre com o primeiro valor: '))
+b = int(input('Entre com o segundo valor: '))
+print(type(a))
+soma = a + b
+subtracao = a - b
+multiplicacao = a * b
+divisao = a / b
+resto = a % b
+print('Soma: {soma}. \nSubtracao: {subtracao}. \nMultiplicacao: {multiplicacao}. '
+      '\nDivisao: {divisao}. \nResto: {resto}'
+      .format(soma=soma, subtracao=subtracao, multiplicacao=multiplicacao,
+              divisao=divisao, resto=resto))
